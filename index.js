1e6fb85127812d59a136e3989d8367525e8d3c2a
deliretik <deliretik@gmail.com>
2021-11-10 14:24:35 +0100

    added method

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express');
-morgan = require('morgan');
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  morgan = require('morgan'),
+  uuid = require('uuid');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 //Morgan middleware library to log all requests
 app.use(morgan('common'));
@@ -14,58 +19,88 @@ app.use((err, req, res, next) => {
 app.use('/documentation.html', express.static('public'));
 
 //Express GET route located at the endpoint “/movies” that returns a JSON object containing data about top 10 movies.
-let top10Movies = [
+let movies = [
+  {
+    movie: 
     {
       title: 'Harry Potter and the Sorcerer\'s Stone',
-      director: 'J.K. Rowling'
-    },
-    {
-      title: 'Lord of the Rings',
-      director: 'J.R.R. Tolkien'
-    },
-    {
-      title: 'Twilight',
-      director: 'Stephanie Meyer'
-    },
-    {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-      director: 'J.K. Rowling'
-    },
-    {
-      title: 'Lord of the Rings',
-      director: 'J.R.R. Tolkien'
-    },
-    {
-      title: 'Twilight',
-      director: 'Stephanie Meyer'
-    },
-    {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-      director: 'J.K. Rowling'
-    },
-    {
-      title: 'Lord of the Rings',
-      director: 'J.R.R. Tolkien'
-    },
-    {
-      title: 'Twilight',
-      director: 'Stephanie Meyer'
-    },
-    {
-    title: 'Twilight',
-      director: 'Stephanie Meyer'
+      genre: 'Thriller',
+      director: {
+        name:'J.K. Rowling',
+        birth: 1980
+      }
     }
+  }];
+//user's data 
+  let user = [
+    {
+      id:1,
+      name: 'omar alaoui',
+      userMovieList: {
+      title: 'Harry Potter and the Sorcerer\'s Stone'
+      }
+    }
+
   ];
-
-
-app.get('/movies', (req, res) => {
-    res.json(top10Movies);
-  });
   
 // GET request that get a textual response
 app.get('/', (req, res) => {
     res.send('Welcome to your Flix');
   });
+
+  // Gets the list of all movies
+  app.get('/movies', (req, res) => {
+    res.json(movies);
+  });
+
+  //get data about a single movie by name
+  app.get('/movies/:movie/:name', (req, res) => {
+    res.json(movies.find((movie) =>
+      { return movie.title === req.params.title}));
+  });
+
+  //New user registration
+  app.post('/user', (req, res) => {
+    let newUser = req.body;
+  
+    if (!newUser.name) {
+      const message = 'Missing name in request body';
+      res.status(400).send(message);
+    } else {
+      newUser.id = uuid.v4();
+      user.push(newUser);
+      res.status(201).send(newUser);
+    }
+  });
+
+  //User informations update
+  app.put('/user/:name', (req, res) => {
+    let user = user.find((user) => { return user.name === req.params.name });
+  
+    if (user) {
+      user.name[req.params.name] = parseInt(req.params.name);
+      res.status(201).send('User ' + req.params.name + ' was assigned a new name ' + req.params.name + ' in ' + req.params.name);
+    } else {
+      res.status(404).send('User with the name ' + req.params.name + ' was not found.');
+    }
+  });
+
+  //user de-registration
+  app.delete('/user/:id', (req, res) => {
+    let user = user.find((user) => { return user.id === req.params.id });
+  
+    if (user) {
+      user = user.filter((obj) => { return obj.id !== req.params.id });
+      res.status(201).send('User ' + req.params.id + ' was deleted.');
+    }
+  });
+
+  //add a movie to user's list of movies
+  app.post('/user/:userMovieList', (req, res) => {
+    let user = req.body;
+
+  });
+
   
   // listen for requests
   app.listen(8080, () => {
