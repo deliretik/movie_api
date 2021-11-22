f20607f05fb4553421c278b1aea86b84c418fd30
deliretik <deliretik@gmail.com>
2021-11-22 11:24:35 +0100

    added mongoose models

---
+++ models.js
@@ -0,0 +1,35 @@
+//requiring mongoose
+const mongoose = require('mongoose');
+
+//movies collection documents schema
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+      Name: String,
+      Description: String
+    },
+    Director: {
+      Name: String,
+      Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+  });
+  
+  //users collection documents schema
+  let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+  
+
+  let Movie = mongoose.model('Movie', movieSchema);
+  let User = mongoose.model('User', userSchema);
+  
+  module.exports.Movie = Movie;
+  module.exports.User = User;
