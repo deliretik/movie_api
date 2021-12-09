62e209bbf255e4dd5b4e7aa4151b37af143853d2
deliretik <deliretik@gmail.com>
2021-12-09 10:48:05 +0100

    added comment for the express validator

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const cors = require('cors');
 app.use(cors());
 let auth = require('./auth')(app);
 
-
+//server side validation with express validator
 const { check, validationResult } = require('express-validator');
 
 //Morgan middleware library to log all requests
