f663d675fb89500bcce0886dc5bd4c2725f52cb6
deliretik <deliretik@gmail.com>
2021-12-10 14:33:02 +0100

    disabled the jwt auth in /movies endpoint

--- index.js
+++ index.js
@@ -71,7 +71,9 @@ app.get('/'), (req, res) => {
   };
 
   // Gets the list of all movies
-  app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+  app.get('/movies', 
+  //passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
